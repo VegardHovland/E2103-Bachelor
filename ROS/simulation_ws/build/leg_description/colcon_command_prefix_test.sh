3d71e07797c33fa8e67bdf665d792068e61f2e0f
Vegard Hovland <vegardbhovland@gmail.com>
2021-02-04 13:50:02 +0100

    prøver å simmulere i ros er en del feil her aka vegards rote proggemappe

---
+++ ROS/simulation_ws/build/leg_description/colcon_command_prefix_test.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/vegard_ubuntu/Documents/E2103-Bachelor/ROS/simulation_ws/build/leg_description/devel/share/leg_description/package.sh"
---
+++ ROS/simulation_ws/build/leg_description/devel/share/leg_description/hook/ros_package_path.ps1
@@ -0,0 +1,3 @@
+# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
+
+colcon_prepend_unique_value ROS_PACKAGE_PATH "$env:COLCON_CURRENT_PREFIX\/home/vegard_ubuntu/Documents/E2103-Bachelor/ROS/simulation_ws/src/leg_description"
