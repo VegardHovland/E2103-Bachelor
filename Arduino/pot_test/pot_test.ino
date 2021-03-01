8430a17588cb067ba7a4deff145e2bf8cccf1dad
Vegard Hovland <vegardbhovland@gmail.com>
2021-03-01 09:07:14 +0100

    test kom

--- Arduino/pot_test/pot_test.ino
+++ Arduino/pot_test/pot_test.ino
@@ -3,23 +3,12 @@
 #define slaveAddr 9
 
 //Constants:
-const int potPin1 = A1; //pin A0 to read analog input
-const int potPin2 = A2; //pin A0 to read analog input
-const int potPin3 = A3; //pin A0 to read analog input
-const int potPin4 = A4; //pin A0 to read analog input
-
-const int comPin1 = 6; //pin A0 to read analog input
-const int comPin2 = 9; //pin A0 to read analog input
-const int comPin3 = 10; //pin A0 to read analog input
-const int comPin4 = 11; //pin A0 to read analog input
+const int potPin = A0; //pin A0 to read analog input
 
 
 //Variables:
-int value1; //save analog value
-int value2; //save analog value1
-int value3; //save analog value
-int value4; //save analog value
-int angle[4];
+float value1; //save analog value
+
 
 
 void setup(){
@@ -27,31 +16,22 @@ void setup(){
 
   Serial.begin(9600);
   Wire.begin(slaveAddr);
+  Wire.onRequest(requestEvent); // register event
   
-  pinMode(potPin1, INPUT); //Optional 
-  pinMode(potPin2, INPUT); //Optional 
-  pinMode(potPin3, INPUT); //Optional 
-  pinMode(potPin4, INPUT); //Optional 
-
+  pinMode(potPin, INPUT); //Optional 
   
 }
 
 void loop(){
-  value1 =  analogRead(potPin1);          //Read and save analog value from potentiometer
-  value1 =  map(value1, 0, 1023, 0, 255); //Map value 0-1023 to 0-255 (PWM)
-
-  value2 =  analogRead(potPin2);           //Read and save analog value from potentiometer
-  value2 =  map(value2, 0, 1023, 0, 255); //Map value 0-1023 to 0-255 (PWM)
-
-  value3 =  analogRead(potPin3);           //Read and save analog value from potentiometer
-  value3 =  map(value3, 0, 1023, 0, 255); //Map value 0-1023 to 0-255 (PWM)
-
-  value4 =  analogRead(potPin4);           //Read and save analog value from potentiometer
-  value4 =  map(value4, 0, 1023, 0, 255); //Map value 0-1023 to 0-255 (PWM)
-
-  angle=[value1, value2, value3,value4];
+  value1 =  analogRead(potPin);          //Read and save analog value from potentiometer
+ 
   
   Serial.println(value1);
   
   delay(100);                             //Small delay
+}
+
+void requestEvent() {
+  Wire.write("hello "); // respond with message of 6 bytes
+  // as expected by master
 }
