a9d357831a562af78af8f0bcbe1dd0ad03eaf4c2
Vegard Hovland <vegardbhovland@gmail.com>
2021-03-02 09:30:33 +0100

    sletter test workspacen

--- ROS/simulation_ws/build/leg_description/devel/share/leg_description/hook/ros_package_path.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value ROS_PACKAGE_PATH "/home/vegard_ubuntu/Documents/E2103-Bachelor/ROS/simulation_ws/src/leg_description"
