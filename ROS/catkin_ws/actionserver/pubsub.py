27edd4efad5f60242acd0b73b639556c543417d6
Vegard Hovland <vegardbhovland@gmail.com>
2021-03-23 22:26:16 +0100

    laget en launchfil i bringup som launcher alt, slettet pythonscript som ikke brukes

--- ROS/catkin_ws/actionserver/pubsub.py
+++ ROS/catkin_ws/actionserver/pubsub.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import rospy
-import numpy as negro
 from std_msgs.msg import Int32
 from sensor_msgs.msg import JointState
 from control_msgs.msg import FollowJointTrajectoryGoal 
@@ -11,48 +10,37 @@ import time
 
 class pubsub(object):
     def __init__(self):
-        global viapoint
-        rospy.init_node('pubsub')
-        #self.viapoint = Int32
+        rospy.init_node('Serial_helper')
         self.value = Float32MultiArray()
+        self.i=0                                                                                   # This should have a better name
         self.value.data = [0, 0, 1.57, 0]
+        self.jointStates = [0, 0, 1.57, 0]
         self.pub = rospy.Publisher('/setpoint2arduino', Float32MultiArray, queue_size=1000)
-        self.joinStates = [0, 0, 1.57, 0]
-        rospy.Subscriber('/robotleg/robotleg_controller/follow_joint_trajectory/goal', FollowJointTrajectoryActionGoal, self.update_value)
-        #rospy.Subscriber('/joint_states', JointState, self.get_joints)
+        rospy.Subscriber('/robotleg/robotleg_controller/follow_joint_trajectory/goal', FollowJointTrajectoryActionGoal, self.update_value)      #define subscriber for trajectory points
+        rospy.Subscriber('/joint_states', JointState, self.get_joints)                              # define joint_states subscriber
         rospy.spin()
 
-    def update_value(self, msg):
-        #self.value = msg.data
-        global A
-        for i in range(len(msg.goal.trajectory.points)):  
-            #if viapoint > len(msg.goal.trajectory.points):
-             # viapoint = 0
-            for j in range(4):
-                self.value.data[j] = msg.goal.trajectory.points[i].positions[j]
-                rospy.loginfo("I received : %f", msg.goal.trajectory.points[i].positions[j])
-            self.pub.publish(self.value)
-            time.sleep(0.2)
-           # A = negro.concatenate((A,msg.goal.trajectory.points[i].positions))
-          #  print(A)
-#
-    #def get_joints(self, msg):
-       # global viapoint
-      #  self.jointStates = msg.position
-       # if(self.tol()):                                                         #Publish setpoints if within tolerance 
-       # self.pub.publish(self.value)
-       # viapoint = viapoint +1                                    #Loop towards next viapoint
-            #rospy.loginfo("I published %i", viapoint )                       # %f", self.value.data
+    def update_value(self, msg):                                                                    # calback function for 
+        while self.i < len(msg.goal.trajectory.points):
+            for j in range(4):                                                                      # loop over 4 joints
+                self.value.data[j] = msg.goal.trajectory.points[self.i].positions[j]                # Update current setpoints     
+            rospy.spin                                                                              # Read new value from sensors
+            time.sleep(0.01)
+            rospy.sleep                                                                             # Sleep for a short duration and continue executing
+            self.pub.publish(self.value)                                                            # Publish current setpoints
+            if self.tol():                                                                          # check if close enough to setpoint
+                self.i = self.i + 1                                                                 # loop to next setpoints in path          
+        self.i=0                                                                                    # resent points index when execution is complete
+
+    def get_joints(self, msg):
+        self.jointStates = msg.position                                                             # Get joint states
     
- #   def tol(self):
-  #      tolerance = True
-   #     for i in range(4):
-    #     if abs(self.value.data[i] - self.jointStates[i]) > 0.01:         ##Check if within tolerance curently 10 degrees
-     #       tolerance = False
-      #  return tolerance
-
-
-
+    def tol(self):
+        tolerance = True
+        for indeks in range(4):
+         if abs(self.value.data[indeks] - self.jointStates[indeks]) > 0.035:                          # Check if within tolerance curently 2 degrees
+            tolerance = False
+        return tolerance
 
 if __name__ == '__main__':
     pubsub()
--- ROS/catkin_ws/actionserver/scripts/full_action_server.py
+++ ROS/catkin_ws/actionserver/scripts/full_action_server.py
@@ -19,11 +19,8 @@ class JointTrajectoryActionServer(object):
 
     def __init__(self, controller_name):
         self._action_ns = controller_name + '/follow_joint_trajectory'
-        self._as = actionlib.SimpleActionServer(
-                self._action_ns,
-                FollowJointTrajectoryAction,
-                execute_cb=self.execute_cb,
-                auto_start = False)
+        self._as = actionlib.SimpleActionServer(self._action_ns,FollowJointTrajectoryAction,execute_cb=self.execute_cb,auto_start = False)
+        self._as.register_goal_callback(self.goalCB)
         self._action_name = rospy.get_name()
         self._as.start()
         self._feedback = FollowJointTrajectoryFeedback
@@ -34,7 +31,9 @@ class JointTrajectoryActionServer(object):
         joint_names = goal.trajectory.joint_names
         trajectory_points = goal.trajectory.points      
         rospy.loginfo(trajectory_points)
-
+        self._goal = self._as.set_succeeded()
+    def goalCB(self):
+        self._goal = self._as.accept_new_goal()
 if __name__ == '__main__':
     rospy.init_node('robotleg_interface')
     server = JointTrajectoryActionServer('robotleg/robotleg_controller')
