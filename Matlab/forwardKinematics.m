0b64681c3cb612ff1580d67b7244e781c9b5f715
Kaosprofet <henrik.moe.arnesen@gmail.com>
2021-03-25 23:14:22 +0100

    Simple via-points angle calculation added, fixed small error in forward kinematics trogonometry test

--- Matlab/forwardKinematics.m
+++ Matlab/forwardKinematics.m
@@ -2,21 +2,33 @@ close all
 clear
 clc
 
-pi=sym(pi);                         % Acurate pi
-%syms theta1 theta2 theta3 theta4 theta5 theta6 theta7;   % for symbolic calulations
-
-
 physicalProportions;                % Including robot physical dimensions
 
-theta2 = 0;
-theta3 = 0;
-theta4 = 0;
-theta5 = 0;
+pi=sym(pi);                         % Acurate pi
+syms theta1 theta2 theta3 theta4 theta5 theta6 theta7;   % for symbolic calulations
+syms a2 a3 a4 a5;
+syms d2 d7;
 
-d       = [0 hipLength 0 0 0 0 phalangesLength-24];                 % z-offset
-theta   = [0 theta2+hipRotation theta3 theta4 theta5 -pi/3 0];      % z-rotation, actuator angles
-a       = [0 hipHeigth femurLength tibiaLength tarsalLength 0 0];   % x-offset
-alpha   = [baseRotation hipRotation 0 0 0 -pi/2 0];                 % x-rotation
+%d2 = hipLength;
+%d7 = phalangesLength-24;
+%theta2 = 0;
+%theta3 = 0;
+%theta4 = 0;
+%theta5 = 0;
+%theta6 = -pi/3;
+%a2 = hipHeigth;
+%a3 = femurLength;
+%a4 = tibiaLength;
+%a5 = tarsalLength;
+alpha1 = baseRotation;
+alpha2 = hipRotation;
+alpha6 = -pi/2;
+
+
+d       = [0 d2 0 0 0 0 d7];                 % z-offset
+theta   = [0 theta2+hipRotation theta3 theta4 theta5 theta6 0];      % z-rotation, actuator angles
+a       = [0 a2 a3 a4 a5 0 0];   % x-offset
+alpha   = [alpha1 alpha2 0 0 0 alpha6 0];                 % x-rotation
 linkType = ["rot" "rot" "rot" "rot" "rot" "rot" "rot"];             % Type of rotation for the Jacobi-matrix
 
 n = length(d);                                                      % degrees of freedom + base rotation and end efector angle
@@ -33,10 +45,13 @@ figure();
 plotRobot(T, n, baseHeight);
 
 %% Mathmatical test
-zLength = a(2)+a(3)+a(4)+a(5)+d(7)*cos(pi/6);
-x = zLength*sin(theta2);
-z = -zLength*cos(theta2)+baseHeight;
-y = -d(2)+a(3)*sin(theta(3))+a(4)*sin(theta(3)+theta(4))+a(5)*sin(theta(3)+theta(4)+theta(5))+d(7)*sin(theta(3)+theta(4)+theta(5)+pi/2-theta(6));
+% Testing to make sure both forward kinematics calculations are correct
+
+delta = pi/2+theta(6); %Theta(6) is allready negative so we use + here
+r = a(2)+a(3)*cos(theta(3))+a(4)*cos(theta(3)+theta(4))+a(5)*cos(theta(3)+theta(4)+theta(5))+d(7)*cos(theta(3)+theta(4)+theta(5)+delta);
+x = r*sin(theta2);
+z = -r*cos(theta2)+baseHeight;
+y = -d(2)+a(3)*sin(theta(3))+a(4)*sin(theta(3)+theta(4))+a(5)*sin(theta(3)+theta(4)+theta(5))+d(7)*sin(theta(3)+theta(4)+theta(5)+delta);
 
 plot3(x,y,z,'kx');
 
--- Matlab/inverseKinematics.m
+++ Matlab/inverseKinematics.m
@@ -19,22 +19,56 @@ z = -520;
 phi= pi/6;
 
 theta(2) = atan2(x,-z)+hipRotation;
-r = sqrt(x^2+z^2);
 
+r = sqrt(x^2+z^2);
 ym = y+d(2)+a(5)*sin(phi+theta(6))-d(7)*sin(pi/2-phi);
 rm = r-a(2)-a(5)*cos(phi+theta(6))-d(7)*cos(pi/2-phi);
 
 gamma = atan2(-ym/sqrt(rm^2+ym^2),-rm/sqrt(rm^2+ym^2));
-
 theta(3) = gamma - acos(-(rm^2+ym^2+a(3)^2-a(4)^2)/(2*a(3)*sqrt(rm^2+ym^2)));
+
 theta(4) = atan2((ym-a(3)*sin(theta(3)))/a(4),(rm-a(3)*cos(theta(3)))/a(4))-theta(3);
 theta(5) = -(phi+theta(3)+theta(4)+theta(6));
 
+figure();
 n = length(d);                                                      % degrees of freedom + base rotation and end efector angle
 A = symCalcA(a, alpha, d, theta, n);
 T = symCalcT(A,n);
 
-
-figure();
 plotRobot(T, n, baseHeight);
 
+%% Generating angles for via points
+
+num = 2;
+x = [linspace(0,0,num) linspace(0,0,num)];
+y = [linspace(150,-100,num) linspace(-100,-200,num)];
+z = [linspace(-520,-520,num) linspace(-520,-420,num)];
+phi = [linspace(pi/6,pi/4,num) linspace(pi/4,pi/3,num)];
+
+m = length(x);
+
+for i=1:m
+    theta1(i) = 0;
+    theta2(i) = atan2(x(i),-z(i))+hipRotation;
+    
+    r = sqrt(x(i)^2+z(i)^2);
+    ym = y(i)+d(2)+a(5)*sin(phi(i)+theta(6))-d(7)*sin(pi/2-phi(i));
+    rm = r-a(2)-a(5)*cos(phi(i)+theta(6))-d(7)*cos(pi/2-phi(i));
+    gamma = atan2(-ym/sqrt(rm^2+ym^2),-rm/sqrt(rm^2+ym^2));
+    
+    theta3(i) = gamma - acos(-(rm^2+ym^2+a(3)^2-a(4)^2)/(2*a(3)*sqrt(rm^2+ym^2)));
+    theta4(i) = atan2((ym-a(3)*sin(theta3(i)))/a(4),(rm-a(3)*cos(theta3(i)))/a(4))-theta3(i);
+    theta5(i) = -(phi(i)+theta3(i)+theta4(i)+theta(6));
+    theta6(i) = -pi/3;
+    theta7(i) = 0;  
+end
+    
+theta = [theta1' theta2' theta3' theta4' theta5' theta6' theta7'];
+
+figure();
+n = length(d);   
+for i =1:m
+    A = symCalcA(a, alpha, d, theta(i,:), n);
+    T = symCalcT(A,n);
+    plotRobot(T, n, baseHeight);
+end
